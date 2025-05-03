937b35a07c8b3460faa0186a16dfea49821c2176
veanderson <118355202+veanderson@users.noreply.github.com>
2025-05-03 14:19:53 +0000

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,7 @@
+carName=input("What is the name of your car?")
+carModel=input("What is the car model?")
+carColor=input("What is your car color?")
+carNumber=input("What is your car number?")
+
+carInfo =(f"My car name is {carName} and the model is {carModel}. The car color is {carColor} and the car number is {carNumber}.");
+print(carInfo)
